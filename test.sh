33289fd8fdbbd5d496bf6f8e962df5292a099c01
tgermain <timothee.germain@wanadoo.fr>
2015-08-04 16:59:05 +0200

    mount local folder into the container

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
-docker run -ti \
--v /home/germaint/dev/logstash-filter-docker-metadata:/tmp/logstash-filter \
--v /var/run/docker.sock:/var/run/docker.sock:ro \
-logstash bash
+#!/bin/bash
+PWD=$(pwd)
+docker run -ti -v $PWD:/tmp/logstash-filter -v /var/run/docker.sock:/var/run/docker.sock:ro logstash bash
